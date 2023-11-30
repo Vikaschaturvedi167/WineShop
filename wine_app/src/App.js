7360c656db8e69dfce2c26c670de09f21e47881c
Vikas Chaturvedi <vc7068318009@gmail.com>
2023-11-30 17:03:56 +0530

    start

--- wine_app/src/App.js
+++ wine_app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { AllRoute } from './routing/AllRoute';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+    <Navbar />
+    <AllRoute />
     </div>
   );
 }
