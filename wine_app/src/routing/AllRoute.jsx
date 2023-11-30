d1cd4f9f90ad0dd96a205c8521cd462ef8b88731
Vikas Chaturvedi <vc7068318009@gmail.com>
2023-11-30 16:42:17 +0530

    start

---
+++ wine_app/src/routing/AllRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import HomePage from '../Pages/HomePage';
+import ProductPage from '../Pages/ProductPage';
+import ProductDetails from '../Pages/ProductDetails';
+import LoginPage from '../Pages/LoginPage.JSX';
+import CartPage from '../Pages/CartPage';
+
+export const AllRoute = () => {
+
+
+  return (
+    <div>
+<Routes>
+<Route path="/" element={<HomePage />} />
+<Route path="/productpage" element={<ProductPage />} />
+<Route path="/productdetails" element={<ProductDetails />} />
+<Route path="/login" element={<LoginPage />} />
+<Route path="/cart" element={<CartPage />} />
+</Routes>
+</div>
+  )
+}
